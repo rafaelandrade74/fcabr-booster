8b9ed51a43da04dd74a9fb94156b8bf3604b0f81
Rafael Andrade da Silva <rafael_andrade74@hotmail.com>
2026-07-22 12:41:31 -0300

    feat: adiciona rota para mensagem do ranking-monitor
    Cria storageKeyExperienceRankingPosition e registra a rota com URL
    sintética fcabr://ranking/experience-position em apiRoutes.

--- src/data/api-routes.js
+++ src/data/api-routes.js
@@ -1,10 +1,15 @@
 import { RouteKeys } from "../data/routekeys.js";
-import { storageKeyGoaRankStatus } from "../content-scripts/routes/profile.js"
+import { storageKeyGoaRankStatus } from "../content-scripts/routes/profile.js";
+import { storageKeyExperienceRankingPosition } from "../content-scripts/routes/ranking.js";
 
 export const apiRoutes = [
     {
         regex: /\/api\/goa-rank-status\?.*oidUser=\d+/,
         storageKey: storageKeyGoaRankStatus
+    },
+    {
+        regex: /^fcabr:\/\/ranking\/experience-position$/,
+        storageKey: storageKeyExperienceRankingPosition
     }
 ];
 
